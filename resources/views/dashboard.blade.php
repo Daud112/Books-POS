6b782c3fa5f8ae61cd96ca5645b576b13914d716
Daud <daud.mushtaq@cognativeaxis.com>
2023-09-29 12:13:45 +0500

    Responsive dashboard and sidebar.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,17 @@
 @include('layouts.partials.header')
 
-@include('layouts.navbar')
 <div class="container-fluid">
-    <div class="row">
-        <div  id="sidebar"  class="col-3 py-5">
+    <header class="page-row row">
+        @include('layouts.navbar')
+    </header>
+    <div class="page-row row page-row-expanded">
+        <div class="col-4 col-sm-4 col-md-3 col-xl-2 sidebar">
             @include('layouts.sidebar')
         </div>
         
-        <div id="content" class="col-9 main">
+        <div id="content" class="col-8 col-sm-8 col-md-9 col-xl-10 main">
             @yield('content')
         </div>
     </div>
-    
 </div>
 @include('layouts.partials.footer')
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
-<div class="sidebar-menus">
-    <a href="#">Home</a>
-    <a href="#">Products</a>
-    <a href="#">POS</a>
-    <a href="{{ route('users') }}">Users</a>
+<div class="sidebar-menus mx-0 mx-sm-0 mx-md-4 mx-lg-5 my-5">    
+    <a class="m-2 m-sm-2 m-md-3 m-xl-3" href="#"   >Home</a>
+    <a class="m-2 m-sm-2 m-md-3 m-xl-3" href="#">Products</a>
+    <a class="m-2 m-sm-2 m-md-3 m-xl-3" href="#">POS</a>
+    <a class="m-2 m-sm-2 m-md-3 m-xl-3" href="{{ route('users') }}">Users</a>
 </div>
