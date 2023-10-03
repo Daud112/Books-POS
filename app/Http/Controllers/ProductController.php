62f5fe51080fdd105f4350b2e74a51b49acb5184
Daud <daud.mushtaq@cognativeaxis.com>
2023-10-03 11:04:56 +0500

    Product create view and controller to save it.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -11,7 +13,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        if(!Auth::check()){
+            return view('auth.login');
+        }
+
+       
+        // return view('admin.user.index', compact('users'));
     }
 
     /**
@@ -19,7 +26,11 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        if(!Auth::check()){
+            return view('auth.login');
+        }
+        
+        return view('admin.product.create');
     }
 
     /**
@@ -27,8 +38,46 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|unique:products',
+            'isbn' => 'required|unique:products|min:13|max:13',
+            'buy_price' => 'required',
+            'sale_price' => 'required',
+            'quantity' => 'required',
+            'cover_image' => 'required|image|mimes:jpeg,png,jpg,gif', // Adjust the validation rules as needed
+        ]);
+        $data = $request->all();
+        
+        $product_data = [
+            'title'  => $data['title'],
+            'author' => $data['author'],
+            'isbn' => $data['isbn'],
+            'genre' => $data['genre'],
+            'buy_price' => $data['buy_price'],
+            'sale_price' => $data['sale_price'],
+            'disc' => $data['disc'],
+            'quantity' => $data['quantity'],
+            'published_date' => $data['published_date'],
+            'publisher' => $data['publisher'],
+        ];
+
+        if ($request->hasFile('cover_image')) {
+            $path = public_path('cover_images/');
+            !is_dir($path) && mkdir($path, 0777, true);
+           
+            $imageName = $data['isbn'] . '.' . $request->file('cover_image')->getClientOriginalExtension();
+            
+            $request->file('cover_image')->move($path, $imageName);
+
+            $product_data['cover_image_path'] = $imageName;
+        }else{
+            $product_data['cover_image_path'] = "default_cover.png";
+        }
+        
+        Product::create($product_data);
+        return view('admin.product.create')->withSuccess('Product is created successfully');;
     }
+    
 
     /**
      * Display the specified resource.
---
+++ resources/views/admin/product/create.blade.php
@@ -0,0 +1,95 @@
+@extends('dashboard')
+@section('content')
+<main class="signup-form">
+    <div class="row d-flex justify-content-center my-5">
+        <h1 class="fs-3 text-dark text-center fw-bold my-3">Create New Product</h1>
+        <form class="row g-3" action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <label for="coverImage" class="form-label">Cover Image</label>
+            <div class="col-md-6">
+                <img id="frame" src="" class="cover-image" />
+            </div>
+            <div class="col-md-9">
+                <div class="input-group col-md-6 mb-3">
+                    <input type="file" class="form-control" id="coverImage" name="cover_image" onchange="preview()">
+                    <label class="input-group-text" for="coverImage">Upload</label>
+                    <button class="btn btn-primary mx-2" onclick="clearImage()">Remove Cover</button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <label for="inputTitle" class="form-label">Title</label>
+                <input type="text" id="inputTitle" class="form-control" placeholder="Title" name="title" aria-label="Title">
+                @if ($errors->has('title'))
+                    <span class="text-danger">{{ $errors->first('title') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputIsbn" class="form-label">ISBN</label>
+                <input type="number" id="inputIsbn" class="form-control" name="isbn" placeholder="International Standard Book Number" aria-label="ISBN">
+                @if ($errors->has('isbn'))
+                    <span class="text-danger">{{ $errors->first('isbn') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputAuther" class="form-label">Auther</label>
+                <input type="text" id="inputAuther" class="form-control" name="author" placeholder="Auther" aria-label="Auther">
+                @if ($errors->has('author'))
+                    <span class="text-danger">{{ $errors->first('author') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputGenre" class="form-label">Genre</label>
+                <input type="text" id="inputGenre" class="form-control" name="genre" placeholder="Such as the epic, tragedy, comedy, novel, and short story" aria-label="Genre">
+                @if ($errors->has('genre'))
+                    <span class="text-danger">{{ $errors->first('genre') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputPublisher" class="form-label">Publisher</label>
+                <input type="text" id="inputPublisher" class="form-control" placeholder="Publisher" name="publisher" aria-label="Publisher">
+                @if ($errors->has('publisher'))
+                    <span class="text-danger">{{ $errors->first('publisher') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputPublisherDate" class="form-label">Publisher Date</label>
+                <input type="date" id="inputPublisherDate" class="form-control" placeholder="Publisher Date" name="published_date" aria-label="Publisher">
+                @if ($errors->has('published_date'))
+                    <span class="text-danger">{{ $errors->first('published_date') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputBuyPrice" class="form-label">Buy Price</label>
+                <input type="number" id="inputBuyPrice" class="form-control" placeholder="Product Buy Price" name="buy_price" aria-label="Buy Price">
+                @if ($errors->has('buy_price'))
+                    <span class="text-danger">{{ $errors->first('buy_price') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputSalePrice" class="form-label">Sale Price</label>
+                <input type="number" id="inputSalePrice" class="form-control" placeholder="Product Sale Price" name="sale_price" aria-label="Sale Price">
+                @if ($errors->has('sale_price'))
+                    <span class="text-danger">{{ $errors->first('sale_price') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputSaleDisc" class="form-label">Sale Discount</label>
+                <input type="number" id="inputSaleDisc" class="form-control" placeholder="Product Sale Disc" name="disc" aria-label="Sale Disc">
+                @if ($errors->has('disc'))
+                    <span class="text-danger">{{ $errors->first('disc') }}</span>
+                @endif
+            </div>
+            <div class="col-md-6">
+                <label for="inputqty" class="form-label">Stock</label>
+                <input type="number" id="inputqty" class="form-control" placeholder="Stock" aria-label="Stock" name="quantity">
+                @if ($errors->has('quantity'))
+                    <span class="text-danger">{{ $errors->first('quantity') }}</span>
+                @endif
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Create Product</button>
+            </div>
+        </form>
+    </div>
+</main>
+@endsection
