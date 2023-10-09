5d329f58e0464190d1e68ab1264ef653fe179e2e
Daud <daud.mushtaq@cognativeaxis.com>
2023-10-09 13:52:38 +0500

    Reduce dublicate code for update bill

--- public/js/app.js
+++ public/js/app.js
@@ -17,32 +17,7 @@ disc_elements.forEach(function (element) {
     var match = classList.match(regex);
 
     if (match) {
-      let sale_product_id = parseInt(match[1]);
-      let bill_item_ele = document.querySelector(".bill-items-list-" + sale_product_id);
-      let bill_item_sale_price_ele = document.querySelector(".bill-item-sale-price--" + sale_product_id);
-      let bill_item_disc_ele = document.querySelector(".bill-item-disc--" + sale_product_id);
-      let bill_item_bill_qty_ele = document.querySelector(".bill-item-qty--" + sale_product_id);
-      let bill_item_bill_total_ele = document.querySelector(".bill-item-total--" + sale_product_id);
-
-      let bill_total_disc_ele = document.querySelector(".bill-item-disc");
-      let bill_total_ele = document.querySelector(".bill-item-total");
-
-      let disc_ele = document.querySelectorAll(".item-disc");
-      let total_price_ele = document.querySelectorAll(".item-total");
-
-      if(bill_item_sale_price_ele && bill_item_disc_ele && bill_item_bill_qty_ele && bill_item_bill_total_ele ){
-        let sale_price_val = bill_item_sale_price_ele.textContent.trim();
-        let disc_val = bill_item_disc_ele.value;
-        let qty_val = bill_item_bill_qty_ele.value;
-        bill_item_bill_total_ele.innerHTML = (sale_price_val-disc_val)*qty_val;
-        
-        var totaldisc = get_input_total(disc_ele);
-        var all_total_price = get_text_total(total_price_ele);
-
-        bill_total_disc_ele.innerHTML = totaldisc;
-        bill_total_ele.innerHTML = all_total_price;
-
-      }
+      update_bill(match, classList)
     } 
     
   });
@@ -56,35 +31,53 @@ qty_elements.forEach(function (element) {
     var match = classList.match(regex);
 
     if (match) {
-      let sale_product_id = parseInt(match[1]);
-      let bill_item_ele = document.querySelector(".bill-items-list-" + sale_product_id);
-      let bill_item_sale_price_ele = document.querySelector(".bill-item-sale-price--" + sale_product_id);
-      let bill_item_disc_ele = document.querySelector(".bill-item-disc--" + sale_product_id);
-      let bill_item_bill_qty_ele = document.querySelector(".bill-item-qty--" + sale_product_id);
-      let bill_item_bill_total_ele = document.querySelector(".bill-item-total--" + sale_product_id);
-
-      let bill_total_qty_ele = document.querySelector(".bill-item-qty");
-      let bill_total_ele = document.querySelector(".bill-item-total");
-
-      let qty_ele = document.querySelectorAll(".item-qty");
-      let total_price_ele = document.querySelectorAll(".item-total");
-
-      if(bill_item_sale_price_ele && bill_item_disc_ele && bill_item_bill_qty_ele && bill_item_bill_total_ele ){
-        let sale_price_val = bill_item_sale_price_ele.textContent.trim();
-        let disc_val = bill_item_disc_ele.value;
-        let qty_val = bill_item_bill_qty_ele.value;
-        bill_item_bill_total_ele.innerHTML = (sale_price_val-disc_val)*qty_val;
-        
-        var totalqty = get_input_total(qty_ele);
-        var all_total_price = get_text_total(total_price_ele);
-
-        bill_total_qty_ele.innerHTML = totalqty;
-        bill_total_ele.innerHTML = all_total_price;
-
-      }
+      update_bill(match, classList)
     }
   });
 });
+
+function update_bill(match, classList){
+  let disc_regex = /item-disc/;
+  let qty_regex = /item-qty/;
+  let disc_match = classList.match(disc_regex);
+  let qty_match = classList.match(qty_regex);
+
+  let sale_product_id = parseInt(match[1]);
+  let bill_item_ele = document.querySelector(".bill-items-list-" + sale_product_id);
+  let bill_item_sale_price_ele = document.querySelector(".bill-item-sale-price--" + sale_product_id);
+  let bill_item_disc_ele = document.querySelector(".bill-item-disc--" + sale_product_id);
+  let bill_item_bill_qty_ele = document.querySelector(".bill-item-qty--" + sale_product_id);
+  let bill_item_bill_total_ele = document.querySelector(".bill-item-total--" + sale_product_id);
+
+  
+
+  if(bill_item_sale_price_ele && bill_item_disc_ele && bill_item_bill_qty_ele && bill_item_bill_total_ele ){
+    let sale_price_val = bill_item_sale_price_ele.textContent.trim();
+    let disc_val = bill_item_disc_ele.value;
+    let qty_val = bill_item_bill_qty_ele.value;
+    bill_item_bill_total_ele.innerHTML = (sale_price_val-disc_val)*qty_val;
+    
+
+    if(qty_match){
+      let bill_total_qty_ele = document.querySelector(".bill-item-qty");
+      let qty_ele = document.querySelectorAll(".item-qty");
+      var totalqty = get_input_total(qty_ele);
+      bill_total_qty_ele.innerHTML = totalqty;
+    }
+
+    if(disc_match){
+      let bill_total_disc_ele = document.querySelector(".bill-item-disc");
+      let disc_ele = document.querySelectorAll(".item-disc");
+      var totaldisc = get_input_total(disc_ele);
+      bill_total_disc_ele.innerHTML = totaldisc;
+    }
+
+    let bill_total_ele = document.querySelector(".bill-item-total");
+    let total_price_ele = document.querySelectorAll(".item-total");
+    var all_total_price = get_text_total(total_price_ele);
+    bill_total_ele.innerHTML = all_total_price;
+  }
+}
 
 
 function get_text_total(ele){
